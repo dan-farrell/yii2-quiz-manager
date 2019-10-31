2dcdd2179d26aa9c77e9ab865809464776d46a40
Dan Farrell <farrell.d@live.co.uk>
2019-10-31 12:21:28 +0000

    clean code

--- views/answer/edit-form.php
+++ views/answer/edit-form.php
@@ -3,17 +3,23 @@
 use yii\bootstrap\Html;
 use yii\widgets\ActiveForm;
 
-$form = ActiveForm::begin([
-  'id' => 'answer',
-  'action' => ['answer/update', 'id' => $id],
-  'options' => [
-    'role' => 'form',
-    'class' => 'answer-form',
-  ],
-]);
+?>
 
-echo 'Edit Answer';
-echo $form->field($answer, 'answer');
-echo Html::submitButton('Save', ['class' => 'btn btn-primary btn-rounded']);
+<div class="col-lg-offset-3 col-md-6">
+  <?php
+  $form = ActiveForm::begin([
+    'id' => 'answer',
+    'action' => ['answer/update', 'id' => $id],
+    'options' => [
+      'role' => 'form',
+      'class' => 'answer-form',
+    ],
+  ]);
 
-ActiveForm::end();
+  echo 'Edit Answer';
+  echo $form->field($answer, 'answer');
+  echo Html::submitButton('Save', ['class' => 'btn btn-primary btn-rounded']);
+
+  ActiveForm::end();
+  ?>
+</div>
--- views/question/edit-form.php
+++ views/question/edit-form.php
@@ -3,17 +3,23 @@
 use yii\bootstrap\Html;
 use yii\widgets\ActiveForm;
 
-$form = ActiveForm::begin([
-  'id' => 'question',
-  'action' => ['question/update', 'id' => $id],
-  'options' => [
-    'role' => 'form',
-    'class' => 'question-form',
-  ],
-]);
+?>
 
-echo 'Edit Question';
-echo $form->field($question, 'name');
-echo Html::submitButton('Save', ['class' => 'btn btn-primary btn-rounded']);
+<div class="col-lg-offset-3 col-md-6">
+  <?php
+  $form = ActiveForm::begin([
+    'id' => 'question',
+    'action' => ['question/update', 'id' => $id],
+    'options' => [
+      'role' => 'form',
+      'class' => 'question-form',
+    ],
+  ]);
 
-ActiveForm::end();
+  echo 'Edit Question';
+  echo $form->field($question, 'name');
+  echo Html::submitButton('Save', ['class' => 'btn btn-primary btn-rounded']);
+
+  ActiveForm::end();
+  ?>
+</div>
